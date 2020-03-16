bf167309736550c10bd8903bc245a051499a3ece
khalid alasmary <khalid@alasmary.com>
2020-03-16 19:12:08 +0300

    deploy

--- index.js
+++ index.js
@@ -6,29 +6,26 @@ dotenv.config({ path: './config/config.env' });
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-try {
-  bot.start(ctx => {
-    ctx.reply(`
+bot.start(ctx => {
+  ctx.reply(`
      أكتب أحرف ترميز الدولة المراد معرفة إحصائيات انتشار الفايروس فيها 
     من الممكن معرفة ترميز الدول من خلال زيارة هذا الموقع:https://www.iban.com/country-codes
     `);
-  });
-  bot.on('message', async ctx => {
-    try {
-      const data = await fetchCasesData(ctx.message.text);
-      ctx.reply(`
-          **تقرير الحالات:**
-          الحالات المُسجلة: ${data.confirmed.value} حالة
-          حالات التعافي: ${data.recovered.value} حالة
-          حالات الوفاة: ${data.deaths.value} حالة
-          `);
-    } catch (err) {
-      const ISO3 = err.response.data.error.message.match(/`(.*?)`/gm)[0];
-      ctx.reply(` لاتوجد بيانات تأكد من إدخال ترميز صحيح(${ISO3})`);
-    }
-  });
+});
+bot.on('message', async ctx => {
+  try {
+    const data = await fetchCasesData(ctx.message.text);
+    ctx.reply(`
+            **تقرير الحالات:**
+            الحالات المُسجلة: ${data.confirmed.value} حالة
+            حالات التعافي: ${data.recovered.value} حالة
+            حالات الوفاة: ${data.deaths.value} حالة
+            `);
+  } catch (err) {
+    console.log(err);
+    const ISO3 = err.response.data.error.message.match(/`(.*?)`/gm)[0];
+    ctx.reply(` لاتوجد بيانات تأكد من إدخال ترميز صحيح(${ISO3})`);
+  }
+});
 
-  bot.launch(); // start
-} catch (error) {
-  console.log(error);
-}
+bot.launch(); // start
